7484bc8888196a94d4bbf299d1a31f1ed6cf08e4
zakaria <zakaria.karkouri@emsi-edu.ma>
2023-02-03 21:10:33 +0100

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ProductsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(db.Products.Find(id));
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(prod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(db.Products.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product prod)
+        {
+            db.Products.Remove(prod);
+            db.SaveChanges(true);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(db.Products.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product prod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Update(prod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="No Product Name")]
+        [Display(Name ="Name")]
+        [MaxLength(15)]
+        [MinLength(5)]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "No Product Price")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "No Product Quantity")]
+        [Range(0,100)]
+        public int Quantity { get; set; }
+    }
+}
